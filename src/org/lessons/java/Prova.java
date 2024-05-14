fc2ea2f53737cfa40237cd5db4db1f2e66af1b98
RodiaGiordano <rodiagiordano93@gmail.com>
2024-05-14 15:02:36 +0200

    prova5

--- src/org/lessons/java/Prova.java
+++ src/org/lessons/java/Prova.java
@@ -4,5 +4,6 @@ public class Prova {
     public static void main(String[] args){
         System.out.println("prova4");
         System.out.println("prova5");
+        System.out.println("prova6");
     }
 }
